93f8ec73951483d3e4091e78d2bb5c6e3a15b5be
kajal shukla <53519201+kajalshukla-kaju@users.noreply.github.com>
2019-11-14 16:05:29 +0530

    third

--- Backend/src/main/java/org/daoImp/UserDaoImp.java
+++ Backend/src/main/java/org/daoImp/UserDaoImp.java
@@ -1,5 +1,47 @@
 package org.daoImp;
 
-public class UserDaoImp {
+import org.Dao.UserDao;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImp implements UserDao {
+	@Autowired
+	SessionFactory sf;
+	
+	@Autowired
+	public UserDaoImp(SessionFactory factory) {
+		System.out.println("User Bean is Created");
+		this.sf=factory;
+	}
+
+	public void insertUser(User user) {
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.saveOrUpdate(user);
+		session.getTransaction().commit();
+		
+	}
+
+	public User findingbyemail(String emailid) {
+		Session session=sf.openSession();
+		User user=null;
+		try {
+			session.beginTransaction();
+			user = session.get(User.class, emailid);
+			session.getTransaction().commit();
+			
+			
+		}catch(HibernateException ex) {
+			System.out.println(ex.getMessage());
+			session.getTransaction().rollback();
+			
+		}
+		return user;
+	}
 
 }
